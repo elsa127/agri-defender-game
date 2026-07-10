3285753bd0e8c5601f0becefb1d06f549d15556c
sindii345 <sindysilvia789@gmail.com>
2026-07-10 20:03:21 +0700

    membuat gameplay dan logika level

--- scenes/GamePlay/GamePlay.gd
+++ scenes/GamePlay/GamePlay.gd
@@ -1,8 +1,12 @@
 extends Node2D
 
+# Slot untuk memasukkan gambar board secara visual lewat Inspector
+@export var gambar_level_1: Texture2D
+@export var gambar_level_2: Texture2D
+@export var gambar_level_3: Texture2D
+
 const DATA_LEVEL = {
 	1: {
-		"gambar_board": "res://asset_gambar/gambar_button/board_lv_1.png",
 		"nama_tanaman": "Padi",
 		"ukuran_grid": "2x2",
 		"target_suhu": 30,
@@ -11,7 +15,6 @@ const DATA_LEVEL = {
 		"kelembapan_awal": 20
 	},
 	2: {
-		"gambar_board": "res://asset_gambar/gambar_button/board_lv_2.png",
 		"nama_tanaman": "Tomat Ceri",
 		"ukuran_grid": "3x3",
 		"target_suhu": 24,
@@ -20,7 +23,6 @@ const DATA_LEVEL = {
 		"kelembapan_awal": 90
 	},
 	3: {
-		"gambar_board": "res://asset_gambar/gambar_button/board_lv_3.png",
 		"nama_tanaman": "Jagung",
 		"ukuran_grid": "3x3",
 		"target_suhu": 28,
@@ -30,6 +32,7 @@ const DATA_LEVEL = {
 	}
 }
 
+# Variabel penentu level yang aktif (biarkan di angka 2 untuk mengetes)
 var level_sekarang: int = 1
 var suhu_saat_ini: int = 0
 var kelembapan_saat_ini: int = 0
@@ -39,14 +42,17 @@ func _ready() -> void:
 
 func muat_level(nomor_level: int) -> void:
 	if not DATA_LEVEL.has(nomor_level):
-		print("Level ", nomor_level, " belum terdaftar!")
 		return
 		
 	var data = DATA_LEVEL[nomor_level]
 	
-	var tekstur_baru = load(data["gambar_board"])
-	if tekstur_baru:
-		$PapanLevel.texture = tekstur_baru
+	# LOGIKA BARU: Mengganti gambar berdasarkan slot export yang diisi manual
+	if nomor_level == 1 and gambar_level_1:
+		$BoardLv1.texture = gambar_level_1
+	elif nomor_level == 2 and gambar_level_2:
+		$BoardLv1.texture = gambar_level_2
+	elif nomor_level == 3 and gambar_level_3:
+		$BoardLv1.texture = gambar_level_3
 	
 	suhu_saat_ini = data["suhu_awal"]
 	kelembapan_saat_ini = data["kelembapan_awal"]
@@ -54,9 +60,4 @@ func muat_level(nomor_level: int) -> void:
 	print("====================================")
 	print("BERHASIL MASUK: LEVEL ", nomor_level)
 	print("Tanaman Level Ini: ", data["nama_tanaman"])
-	print("Ukuran Kotak Grid: ", data["ukuran_grid"])
-	print("------------------------------------")
-	print("KONDISI TANTANGAN AWAL:")
-	print("Suhu Sekarang     : ", suhu_saat_ini, "°C (Harusnya pas: ", data["target_suhu"], "°C)")
-	print("Kelembapan Sekarang: ", kelembapan_saat_ini, "% (Harusnya pas: ", data["target_kelembapan"], "%)")
 	print("====================================")
